700773ea710d6285915bf1d844b045b2cb4f8f6b
Albamar Flores <89668743+Albamarfdc@users.noreply.github.com>
2025-10-07 19:54:53 -0300

    Update db.js

--- SERVER/src/database/db.js
+++ SERVER/src/database/db.js
@@ -2,11 +2,16 @@ import pg from 'pg'
 import 'dotenv/config'
 
 const { Pool } = pg
-const { DB_URL } = process.env
+const {DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE } = process.env
 
 const config = {
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  allowExitOnIdle: true,
 
-  connectionString: DB_URL
+  // connectionString: DB_URL
 }
 
 const pool = new Pool(config)
