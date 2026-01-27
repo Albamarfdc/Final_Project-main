780dbb2a3b09f0abe40d1001208a7755ca22a482
Albamar Flores <89668743+Albamarfdc@users.noreply.github.com>
2026-01-27 15:29:28 -0300

    Add DB_URL to database connection configuration

--- SERVER/src/database/db.js
+++ SERVER/src/database/db.js
@@ -2,7 +2,7 @@ import pg from 'pg'
 import 'dotenv/config'
 
 const { Pool } = pg
-const {DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE } = process.env
+const {DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE,DB_URL } = process.env
 
 const config = {
   host: DB_HOST,
